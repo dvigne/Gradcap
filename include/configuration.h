09f07db53f9121eca4f7bbcfd20c6c1e0f322400
Derick Vigne <derickvigne@me.com>
2021-04-11 19:21:06 -0400

    Added Configuration Include

---
+++ include/configuration.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/////////////////////////////////////////////////
+/// Wifi Configuration
+/////////////////////////////////////////////////
+
+// Set the Network Name
+#define SSID "Gradcap"
+
+// Set the network password (if blank open network)
+#define PASSWORD ""
+
+// Make the network hidden?
+#define HIDDEN false
+
+// Maximum number of simultaneous connected clients (0 - 8)
+#define MAX_CLIENTS 4
+
+// Set the wifi channel (Useful if in congested networks)
+#define CHANNEL 6
+
+
+/////////////////////////////////////////////////
+/// Webserver Configuration
+/////////////////////////////////////////////////
+
+// Domain name of the webserver -- also used in DNS requests
+#define DOMAIN_NAME "gradcap.io"
+
+// Webserver Port
+#define PORT 80
+
+/////////////////////////////////////////////////
+/// Pinout Configuration
+/////////////////////////////////////////////////
+
+// TODO
