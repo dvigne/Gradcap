c264a3c118c525a539e884a63468cf4b03389668
Derick Vigne <derickvigne@me.com>
2021-04-30 20:28:30 -0400

    Added LED Control Configuration Keys

--- include/configuration.h
+++ include/configuration.h
@@ -34,4 +34,16 @@
 /// Pinout Configuration
 /////////////////////////////////////////////////
 
-// TODO
+// Neopixel Data Control Pin
+#define LED_CONTROL_PIN 5
+
+// LED Count(For Addressing)
+#define LED_COUNT 26
+
+//  Pixel Type:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+#define PIXEL_TYPE NEO_GRB + NEO_KHZ800
